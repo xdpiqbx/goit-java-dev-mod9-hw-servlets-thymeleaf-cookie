cf76920059296a21959ae63d87d20558e9064f05
xdpiqbx <xdpiqbx@gmail.com>
2022-12-24 12:10:17 +0200

    task 1 done

--- app/src/main/java/com/dpiqb/servlets/TimeServlet.java
+++ app/src/main/java/com/dpiqb/servlets/TimeServlet.java
@@ -1,5 +1,11 @@
 package com.dpiqb.servlets;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.servlet.JavaxServletWebApplication;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,10 +14,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Map;
 
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
   // http://localhost:8080/time
+  private TemplateEngine engine;
+  public void init() throws ServletException {
+    ServletContext servletContext = getServletContext();
+    JavaxServletWebApplication application = JavaxServletWebApplication.buildApplication(servletContext);
+    WebApplicationTemplateResolver resolver = new WebApplicationTemplateResolver(application);
+
+    resolver.setPrefix("/WEB-INF/templates/");
+    resolver.setSuffix(".html");
+    resolver.setTemplateMode("HTML5");
+    resolver.setCacheable(false);
+
+    engine = new TemplateEngine();
+    resolver.setOrder(engine.getTemplateResolvers().size());
+    engine.addTemplateResolver(resolver);
+  }
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
     resp.setContentType("text/html; charset=utf-8");
@@ -31,7 +53,12 @@ public class TimeServlet extends HttpServlet {
       .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss O"))
       .replace("GMT", "UTC");
 
-    resp.getWriter().write("<h4>" + currentUtc + "</h4>");
+    Context context = new Context(
+      req.getLocale(),
+      Map.of("time", currentUtc)
+    );
+
+    engine.process("time", context, resp.getWriter());
     resp.getWriter().close();
   }
 }
